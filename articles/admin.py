8ba4d9a8960be76562471cd0ce6b3995d20f0cc5
Sajen Tuladhar <sajentuladhar9@gmail.com>
2024-02-21 15:25:04 +0545

    made migrations and configured amdin panel

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Article
 # Register your models here.
+
+admin.site.register(Article) # lets django know that we want to see article in the admin section
--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)  
+    slug  = models.SlugField()
+    body  = models.TextField()
+    date  = models.DateTimeField(auto_now_add=True) # when an article is created it is automatically populate this field
+    #add in thumbnail later
+    #add in author later
+    
+    def __str__(self) : # displays the string representation of fields
+        return self.title
