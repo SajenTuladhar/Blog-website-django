bbeb10e740e8de3f1fd0a8039ad2c752cc21a4a8
Sajen Tuladhar <sajentuladhar9@gmail.com>
2024-02-26 14:24:33 +0545

    login and signup fucntions

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import login
 
 # Create your views here.
 def signup_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid(): # checks if the value inserted by user is correct and meets requirement or not
-         form.save() #creates a new user for us
-         #log the user in
+         form.save() #creates a new user for us and returns it to us
+         user = form.save()
+         login(request,user)
          return redirect('articles:list') #name of the app is article tha we defined and the list is the name of the url that we defined as well
     else:
         form = UserCreationForm()
@@ -17,6 +19,9 @@ def login_view(request):
     if request.method == 'POST':
         form= AuthenticationForm(data=request.POST)
         if form.is_valid():
+            #log user in
+            user = form.get_user() #retriving the user
+            login(request,user) #logging the user in
             return redirect("articles:list")
     
     else:
